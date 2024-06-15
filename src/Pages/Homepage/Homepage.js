f6f1f74ba04d5e791ab22617cf8cb323c2a57afb
genti180302 <gentian_1@live.at>
2024-06-15 15:29:49 +0200

    finished project

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -2,24 +2,14 @@ import React from "react";
 import Header from "../../components/Header/header";
 import CryptoSlide from "../../components/CryptoSlide/CryptoSlide";
 import ExchangeSlide from "../../components/ExchangeSlide/ExchangeSlide";
-import styled, { keyframes } from 'styled-components';
 import AnimationCrypto from "../../components/Animation_Crypto/animation";
-import Animation_exchange from "../../components/Animation_Exchange/animation_exchange";
 import Footer from "../../components/Footer/footer";
 import CryptoCurrencyConverter from "../../components/Cryptocurrency_Converter/CryptoCurrencyConverter";
+import AnimationExchange from "../../components/Animation_Exchange/AnimationExchange";
 
 
 
 const Homepage = () => {
-
-    // const horizontalScroll = keyframes;
-    //
-    // const ImageContainer = styled.div;
-    //
-    // const Wrapper = styled.div;
-    //
-    // const Image = styled.img
-
 
     return(
         <>
@@ -28,7 +18,7 @@ const Homepage = () => {
             <CryptoSlide/>
             <ExchangeSlide/>
             <CryptoCurrencyConverter/>
-            <Animation_exchange/>
+            <AnimationExchange/>
             <Footer/>
         </>
     )
--- src/components/CoinDetails/Coindetails.js
+++ src/components/CoinDetails/Coindetails.js
@@ -1,9 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {Link, useParams} from 'react-router-dom';
 import fetch from "node-fetch";
-import {ApiKey, ApiUrl} from "../../database/apiurl";
 import './Coindetails.scss'
-import AnimationCrypto from "../Animation_Crypto/animation";
 
 const CoinDetailPage = () => {
     const {id} = useParams();
@@ -98,40 +96,4 @@ const CoinDetailPage = () => {
 
 export default CoinDetailPage;
 
-// useEffect(() => {
-//     fetch(`https://data-api.cryptocompare.com/asset/v1/data/by/id?asset_id=${id}` , {
-//         method: 'GET',
-//         headers: {
-//             'accept': 'application/json',
-//             'authorization': 'f92836cc7e636a6cd55f0a6e8715d038e49de6a50fc45ea6bd86ad7a98cb6ecc',
-//         }
-//     })
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data)
-//             setCoinDetails(data.Data);
-//         })
-//         .catch(error => console.log(error));
-// }, [id]);
-
-
-// <div className={'MarketDataStyle'}>
-//     <h1 style={{marginTop: '20px', paddingLeft: '45px'}}> Market Data </h1>
-//     <div className={'DetailsItem'}> Price
-//         <p className={'DetailsValueText'}>{coinDetails.Data.PRICE_USD.toLocaleString()}$ </p>
-//     </div>
-//     <div className={'DetailsItem'}> MarketCap
-//         <p className={'DetailsValueText'}>{coinDetails.Data.CIRCULATING_MKT_CAP_USD.toLocaleString()} </p>
-//     </div>
-//     <div className={'DetailsItem'}> Symbol
-//         <p style={{marginRight: '20px'}} className={'DetailsValueText'}>{coinDetails.Data.SYMBOL} </p>
-//     </div>
-//     <div className={'DetailsItem'}> 24H Change
-//         <p className={'DetailsValueText'}> {coinDetails.Data.SPOT_MOVING_24_HOUR_CHANGE_PERCENTAGE_USD.toFixed(2)}% </p>
-//     </div>
-//     <div className={'DetailsItem'}> White Paper
-//         <p style={{marginRight: '20px'}} className={'DetailsValueText'}><a
-//             href={coinDetails.Data.WHITE_PAPER_URL}> Click here </a></p>
-//     </div>
-// </div>
 
